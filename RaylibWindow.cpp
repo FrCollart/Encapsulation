9a98f5f3ab7bc85327d9c34c365905d523b9bc49
FrCollart <fcollart@gaming.tech>
2024-11-18 11:56:04 +0100

    Added Raylib implementation

--- RaylibWindow.cpp
+++ RaylibWindow.cpp
@@ -1,1 +1,46 @@
 #include "RaylibWindow.h"
+#include "raylib.h"
+#include "Sprite.h"
+
+void RaylibWindow::Initialize()
+{
+
+}
+
+void RaylibWindow::CreateWindow(int width, int height, const char* title)
+{
+	InitWindow(width, height, title);
+}
+
+bool RaylibWindow::IsOpen() const
+{
+	return !WindowShouldClose();
+}
+
+void RaylibWindow::Clear()
+{
+	ClearBackground(WHITE);
+}
+
+void RaylibWindow::Draw(const std::vector<class Sprite>& spritesList)
+{
+	BeginDrawing();
+	for (const auto& sprite : spritesList)
+	{
+		InternalDrawSprite(sprite);
+	}
+	EndDrawing();
+}
+
+void RaylibWindow::InternalDrawSprite(const Sprite& sprite)
+{
+	const void* data = sprite.GetData();
+	Texture2D* texture = (Texture2D*)data;
+
+	DrawTexture(*texture, sprite.GetX(), sprite.GetY(), WHITE);
+}
+
+void RaylibWindow::Cleanup()
+{
+	CloseWindow();
+}
--- RaylibWindow.h
+++ RaylibWindow.h
@@ -3,6 +3,16 @@
 
 class RaylibWindow : public Window
 {
+public:
+    void Initialize() override;
+    void CreateWindow(int width, int height, const char* title) override;
+    bool IsOpen() const override;
+    void Clear() override;
+    void Draw(const std::vector<class Sprite>& spritesList) override;
+    void Cleanup() override;
+
+private:
+    void InternalDrawSprite(const Sprite& sprite);
 
 };
 
