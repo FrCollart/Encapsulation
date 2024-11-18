ce76e37ccf3702dd34dc4b82b26fd67f402c0e05
FrCollart <fcollart@gaming.tech>
2024-11-18 11:56:09 +0100

    Update SDLWindow.h

--- SDLWindow.h
+++ SDLWindow.h
@@ -5,12 +5,12 @@ class SDLWindow : public Window
 {
 
 public:
-    virtual void Initialize() override;
-    virtual void CreateWindow(int width, int height, const char* title) override;
-    virtual bool IsOpen() const override;
-    virtual void Clear() override;
-    virtual void Draw(const std::vector<class Sprite>& spritesList) override;
-    virtual void Cleanup() override;
+    void Initialize() override;
+    void CreateWindow(int width, int height, const char* title) override;
+    bool IsOpen() const override;
+    void Clear() override;
+    void Draw(const std::vector<class Sprite>& spritesList) override;
+    void Cleanup() override;
 
 private:
     void InternalDrawSprite(const class Sprite& sprite);
