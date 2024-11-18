c6e6910da700d0a974b1cec92206d21d56ffb38c
FrCollart <fcollart@gaming.tech>
2024-11-18 10:43:14 +0100

    Update Sprite.h

--- Sprite.h
+++ Sprite.h
@@ -1,12 +1,21 @@
 #pragma once
+#include <vector>
 
 class Sprite {
 
 public:
+    Sprite() : x(0), y(0), width(1), height(1){}
+    Sprite(int inX, int inY) : x(inX), y(inY), width(1), height(1){}
+    Sprite(int inX, int inY, int inWidth, int inHeight) : x(inX), y(inY), width(inWidth), height(inHeight){}
     virtual ~Sprite() = default;
 
     virtual void LoadImage(const char* filepath) = 0;
     virtual const void* GetData() const = 0;
+
+    int GetX() const { return x; }
+    int GetY() const { return y; }
+    int GetWidth() const { return width; }
+    int GetHeight() const { return height; }
 
     void SetPosition(int posX, int posY) {
         x = posX;
@@ -16,5 +25,7 @@ public:
 protected:
     int x;
     int y;
+    int width;
+    int height;
 };
 
