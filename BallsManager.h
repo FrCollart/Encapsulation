6713d12de5e2c00a57b14767e3d37e8250420f08
FrCollart <fcollart@gaming.tech>
2024-11-19 09:59:22 +0100

    Update BallsManager.h

--- BallsManager.h
+++ BallsManager.h
@@ -1,5 +1,16 @@
 #pragma once
+#include <vector>
+
 class BallsManager
 {
+public:
+	void Initialize();
+	void Update();
+
+	std::vector<class Ball*> GetBalls() const { return balls; }
+
+private:
+
+	std::vector<class Ball*> balls;
 };
 
