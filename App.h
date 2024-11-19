6286a6c001236095e6b90f07abec5b1871e38ffb
FrCollart <fcollart@gaming.tech>
2024-11-19 16:42:39 +0100

    Added cool textures

--- App.h
+++ App.h
@@ -37,5 +37,5 @@ private:
 constexpr int WINDOW_WIDTH = 800;
 constexpr int WINDOW_HEIGHT = 600;
 constexpr const char* WINDOW_TITLE = "Ligma";
-constexpr const char* TEXTURE_PATH = "Textures/ball.png";
+constexpr const char* TEXTURE_PATH = "Textures/PoolBall1.png";
 constexpr const char* FONT_PATH = "Fonts/Roboto-Black.ttf";
