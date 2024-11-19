db32c0dde5f0972cac3e2534696d97b2e7f7284d
FrCollart <fcollart@gaming.tech>
2024-11-19 10:07:29 +0100

    messed up

--- DisplayableObject.h
+++ DisplayableObject.h
@@ -2,13 +2,12 @@
 
 class DisplayableObject
 {
-
 public:
 
 	DisplayableObject(int x, int y, class Sprite* sprite) : x(x), y(y), sprite(sprite) {}
 	float GetX() const { return x; }
-	float GetY() const { return x; }
-	int GetSprite() const { return x; }
+	float GetY() const { return y; }
+	Sprite* GetSprite() const { return sprite; }
 
 protected:
 	float x;
